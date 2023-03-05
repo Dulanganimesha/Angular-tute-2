732438cece879417bd14f52266053d17db1e59dd
dulanganimesha <dulanganimesha@users.noreply.github.com>
2023-03-05 19:37:31 +0530

    add ngFor with index

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,11 +7,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   showSecret = false;
- log: number[] = [];
+  log: any[] = [];
 
   onToggleDetails(){
     this.showSecret = !this.showSecret;
-    this.log.push(this.log.length + 1);
+    // this.log.push(this.log.length + 1);
+    this.log.push(new Date());
   }
 
 
